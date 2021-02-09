661e8ef1053aa4f13af9812653979811777981de
Bernhard Suttner <suttner@atix.de>
2021-02-09 10:57:56 +0100

    Automatically assign template to default organization / location

--- db/seeds.d/75-job_templates.rb
+++ db/seeds.d/75-job_templates.rb
@@ -4,7 +4,10 @@ User.as_anonymous_admin do
   JobTemplate.without_auditing do
     Dir[File.join("#{ForemanAcd::Engine.root}/app/views/templates/**/*.erb")].each do |template|
       sync = !Rails.env.test? && Setting[:remote_execution_sync_templates]
-      JobTemplate.import_raw!(File.read(template), :default => true, :locked => true, :update => sync)
+      template = JobTemplate.import_raw!(File.read(template), :default => true, :locked => true, :update => sync)
+
+      template.organizations << Organization.unscoped.all if template&.organizations&.empty?
+      template.locations << Location.unscoped.all if template&.locations&.empty?
     end
   end
 end
